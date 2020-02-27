9fc28a8e6f15e306c4dc4d686f782168ab7400dd
houyuewei <houyaowei@163.com>
2020-02-27 22:42:20 +0800

    增加策略模式文档

--- chapter04/code/4.1DesignPattern/Strategy/main.js
+++ chapter04/code/4.1DesignPattern/Strategy/main.js
@@ -4,8 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * 测试方法
  */
 var Context_1 = require("./Context");
-var StrategyBImpl_1 = require("./StrategyBImpl");
-// const context = new Context(new StrategyA());
-// context.executeStrategy();
-var context = new Context_1.default(new StrategyBImpl_1.default());
+var StrategyAImpl_1 = require("./StrategyAImpl");
+var context = new Context_1.default(new StrategyAImpl_1.default());
 context.executeStrategy();
+// const context = new Context(new StrategyB()); context.executeStrategy();
--- chapter04/code/4.1DesignPattern/Strategy/main.ts
+++ chapter04/code/4.1DesignPattern/Strategy/main.ts
@@ -5,8 +5,7 @@ import Context from "./Context";
 import StrategyA from "./StrategyAImpl";
 import StrategyB from "./StrategyBImpl";
 
-// const context = new Context(new StrategyA());
-// context.executeStrategy();
+// const context = new Context(new StrategyA()); context.executeStrategy();
 
 const context = new Context(new StrategyB());
 context.executeStrategy();
