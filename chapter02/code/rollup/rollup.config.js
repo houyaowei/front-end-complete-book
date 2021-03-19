1a8636fb45a470d44416a8e6a0c24f2cfb7ac492
houyuewei <houyaowei@163.com>
2021-03-19 09:58:44 +0800

    rollup demo update

--- chapter02/code/rollup/rollup.config.js
+++ chapter02/code/rollup/rollup.config.js
@@ -1,7 +1,9 @@
-import babel from "rollup-plugin-babel";
+import babel from "rollup-plugin-babel"; //
 import { eslint } from 'rollup-plugin-eslint';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve'; //配合resolve包使用
+import commonjs from 'rollup-plugin-commonjs'; //将 commonjs 模块转成 es6 模块
+import replace from "rollup-plugin-replace";
+import uglify from "rollup-plugin-uglify";
 
 module.exports={
   input: "src/scripts/main.js",
@@ -12,7 +14,7 @@ module.exports={
   },
   plugins: [
     resolve({
-      jsnext: true,
+      jsnext: true,  //node ->ES6
       main: true,
       browser: true,
     }),
@@ -24,5 +26,9 @@ module.exports={
     babel({
       exclude: 'node_modules/**',
     }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV) // 查找ENV,并用NODE_ENV替换
+    }),
+    process.env.NODE_ENV === "production" && uglify()
   ]
 }
