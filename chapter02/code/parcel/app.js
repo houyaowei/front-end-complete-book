7e2a8a48a5e518f5c0a3a39bc0ee9cfd4b696c10
houyuewei <houyaowei@163.com>
2021-03-21 23:03:54 +0800

    parcel demo update

--- chapter02/code/parcel/app.js
+++ chapter02/code/parcel/app.js
@@ -1,6 +1,6 @@
 import name from "./src/utils/index"
 import ss from "./src/scripts/index"
-import "./style.css"
+// import "./style.css"
 
 document.writeln("hello,parcel<br>")
 document.writeln("project name :", name.projectName ,"<br>")
